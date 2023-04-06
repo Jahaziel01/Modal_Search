d937603378b24a738ec21102ab2df3079aaacd12
Jahaziel Martinez <Jahaziel Martinez@genuineti.local>
2023-04-06 11:29:42 -0400

    filter 2.0

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,13 @@ function App() {
   const [data, setData] = useState([])
   const [countriesFilter, setCountriesFilter] = useState([])
 
+
+  //Api Countries
   useEffect(() => {
     const Data = async () => {
       try {
-
         const result = await fetch('https://countriesnow.space/api/v0.1/countries/currency')
         const countries = await result.json()
-
         const countriesState = countries.data.map(countrie => countrie.name)
         setData(countriesState)
 
@@ -25,24 +25,7 @@ function App() {
     Data()
   }, [])
 
-
-  const handleChange = e => {
-    setSearch(e.target.value);
-    filtrar(e.target.value);
-  }
-
-  const filtrar = (terminoBusqueda) => {
-    var resultadosBusqueda = data.filter((elemento) => {
-      if (elemento.toString().toLowerCase().includes(terminoBusqueda.toLowerCase())
-      ) {
-        return elemento;
-      }
-    });
-    setCountriesFilter(resultadosBusqueda);
-  }
-
-  console.log(countriesFilter)
-
+  //Modal
   useEffect(() => {
     if (search.length > 0) {
       setOpenModal(true)
@@ -59,7 +42,28 @@ function App() {
     }
   }
 
-  console.log(search)
+  //Filter Countries
+  const handleChange = e => {
+    setSearch(e.target.value);
+    filtrar(e.target.value);
+  }
+
+  const filtrar = (terminoBusqueda) => {
+    var resultadosBusqueda = data.filter((elemento) => {
+      if (elemento.toString().toLowerCase().includes(terminoBusqueda.toLowerCase())
+      ) {
+        return elemento;
+      }
+    });
+    setCountriesFilter(resultadosBusqueda);
+  }
+
+  //Clear
+  const HandleClear = () => {
+    setSearch('')
+    setCountriesFilter([])
+  }
+
 
   return (
     <div>
@@ -73,17 +77,40 @@ function App() {
       <div>
         <button onClick={handleModal}>Open Modal</button>
 
-        <div id="myModal" className={openModal ? 'modal' : 'hidden'}>
-          <div className="modal-content">
+        <div id="myModal" className={openModal ? 'modal' : 'modalOff'}>
+          <div className="container">
             <span className="close" onClick={handleModal}>&times;</span>
-            <p>Ciudad</p>
-            <span>{countriesFilter}</span>
-            <input
-              type='search'
-              placeholder='Ingrese el BL'
-              onChange={handleChange}
-              value={search}
-            />
+            <div className='search'>
+              <div className='search_head'>
+                <input
+                  type='search'
+                  value={search}
+                  onChange={handleChange}
+                />
+                <svg onClick={HandleClear} xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-trash" width="26" height="26" viewBox="0 0 24 24" strokeWidth="2" stroke="#ff2825" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <line x1="4" y1="7" x2="20" y2="7" />
+                  <line x1="10" y1="11" x2="10" y2="17" />
+                  <line x1="14" y1="11" x2="14" y2="17" />
+                  <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+                  <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+                </svg>
+              </div>
+              <div className='content'>
+
+                {countriesFilter.length ?
+                  <ul>
+                    {
+                      countriesFilter.map(countrie => (
+                        <li><a href='#'>{countrie}</a></li>
+                      ))
+                    }
+                  </ul>
+                  : <p>Sin resultados</p>
+                }
+
+              </div>
+            </div>
           </div>
         </div>
 
